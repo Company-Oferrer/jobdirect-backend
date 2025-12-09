62555253af864e78abedf12113b01984718a25b4
Oman Ferrer <oman.ferrer@gmail.com>
2025-12-08 23:45:40 -0500

    Add CORS support and update dependencies

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const pool = require('./db');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 /**
