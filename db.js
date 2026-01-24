ccacf7250679208757aaf7620dc3a4e88ad159a3
josegm13 <josegm13@gmail.com>
2026-01-23 21:57:18 -0500

    Add SSL configuration to database connection

--- db.js
+++ db.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 
 const pool = new Pool({
   connectionString: 'postgresql://jobdirectadmin:Proyectos123@pg-jobdirect-dev.postgres.database.azure.com:5432/jobdirect',
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
